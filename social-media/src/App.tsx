bd3132e649e83ab9f33eb44d9ac98e1da261cc11
Miloss1970 <134635415+Miloss1970@users.noreply.github.com>
2024-07-02 14:34:41 +0200

    setup routing

--- social-media/src/App.tsx
+++ social-media/src/App.tsx
@@ -1,10 +1,22 @@
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
-    <>
-      <p className="font-bold">bla</p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/home" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
