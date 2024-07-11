b4f73620648766f3b661f506f7599d1dbb3749ca
Miloss1970 <134635415+Miloss1970@users.noreply.github.com>
2024-07-11 14:52:04 +0200

    create audio player i voiceRecorder

--- social-media/src/components/audioPlayer/AudioPlayer.tsx
+++ social-media/src/components/audioPlayer/AudioPlayer.tsx
@@ -1,7 +1,100 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
+import { FaPlay, FaStop } from "react-icons/fa";
 
-const AudioPlayer = () => {
-  return <div>AudioPlayer</div>;
+interface AudioPlayerProps {
+  audio: string;
+}
+
+const AudioPlayer: React.FC<AudioPlayerProps> = ({ audio }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState<number>(0);
+  const audioRef = useRef<HTMLAudioElement>(new Audio(audio));
+  const animationRef = useRef<number | null>(null);
+  const progressBarRef = useRef<HTMLSpanElement>(null);
+  const togglePlay = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const audio = audioRef.current;
+
+    audio.addEventListener("timeupdate", () => {
+      if (audio.duration) {
+        setDuration(audio.duration);
+      }
+    });
+    const updateProgressBar = () => {
+      if (progressBarRef.current) {
+        const progress = (audio.currentTime / duration) * 100;
+        progressBarRef.current.style.left = `${progress}%`;
+        setCurrentTime(audio.currentTime);
+      }
+      animationRef.current = requestAnimationFrame(updateProgressBar);
+    };
+
+    if (isPlaying) {
+      animationRef.current = requestAnimationFrame(updateProgressBar);
+    } else {
+      if (animationRef.current) {
+        cancelAnimationFrame(animationRef.current);
+      }
+    }
+
+    audio.addEventListener("ended", () => {
+      setIsPlaying(false);
+      setCurrentTime(0);
+      if (progressBarRef.current) {
+        progressBarRef.current.style.left = `0%`;
+      }
+    });
+
+    return () => {
+      if (animationRef.current) {
+        cancelAnimationFrame(animationRef.current);
+      }
+      audio.removeEventListener("timeupdate", () => {});
+      audio.removeEventListener("ended", () => {});
+    };
+  }, [isPlaying, duration]);
+
+  const formatTime = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60);
+    const secs = Math.floor(seconds % 60);
+    return `${minutes}:${secs < 10 ? "0" : ""}${secs}`;
+  };
+
+  return (
+    <div className="flex gap-3  items-center w-full py-[24px] mt-[15px] rounded-md px-[10px] bg-[#EFEFEF]">
+      <button onClick={togglePlay}>
+        {isPlaying ? (
+          <span className="w-[35px] h-[35px] flex justify-center items-center rounded-full bg-[#e61313]">
+            <FaStop className="text-white text-[16px]" />
+          </span>
+        ) : (
+          <span className="w-[35px] h-[35px] flex justify-center items-center rounded-full bg-figmaBlue">
+            <FaPlay className="text-white text-[16px]" />
+          </span>
+        )}
+      </button>
+      <div className=" w-full relative h-[3px] bg-figmaGrayShade2">
+        <span
+          ref={progressBarRef}
+          className="absolute w-[2px] h-[82px] bg-[#e61313] top-[-39px]  left-0 bottom-[-30px]"
+        />
+      </div>
+      <div className="flex items-center text-figmaGrayShade">
+        <p>0{formatTime(currentTime)}/</p>
+        <p>
+          {duration && duration !== Infinity ? formatTime(duration) : "00:00"}
+        </p>
+      </div>
+    </div>
+  );
 };
-
 export default AudioPlayer;
--- social-media/src/components/voiceRecorder/VoiceRecorder.tsx
+++ social-media/src/components/voiceRecorder/VoiceRecorder.tsx
@@ -1,7 +1,180 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import AudioPlayer from "../audioPlayer/AudioPlayer";
+import { FaStop, FaMicrophone } from "react-icons/fa";
 
-const VoiceRecorder = () => {
-  return <div>VoiceRecorder</div>;
+interface AudioVisualizerProps {
+  isRecording: boolean;
+  setIsRecording: React.Dispatch<React.SetStateAction<boolean>>;
+  audioURL: string | null;
+  setAudioURL: React.Dispatch<React.SetStateAction<string | null>>;
+  setAudioBlob: React.Dispatch<React.SetStateAction<Blob | null>>;
+}
+
+const AudioVisualizer: React.FC<AudioVisualizerProps> = ({
+  isRecording,
+  setIsRecording,
+  audioURL,
+  setAudioURL,
+  setAudioBlob, // Novi prop
+}) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
+  const [, setAnalyser] = useState<AnalyserNode | null>(null);
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
+    null
+  );
+  const [, setDataArray] = useState<Uint8Array | null>(null);
+  const [microphoneAllowed, setMicrophoneAllowed] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (isRecording) {
+      startRecording();
+    } else {
+      stopRecording();
+    }
+  }, [isRecording]);
+
+  const startRecording = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
+      const audioContext = new AudioContext();
+      const analyser = audioContext.createAnalyser();
+      analyser.fftSize = 256;
+
+      const bufferLength = analyser.frequencyBinCount;
+      const dataArray = new Uint8Array(bufferLength);
+
+      const source = audioContext.createMediaStreamSource(stream);
+      source.connect(analyser);
+
+      setAudioContext(audioContext);
+      setAnalyser(analyser);
+      setDataArray(dataArray);
+
+      const mediaRecorder = new MediaRecorder(stream);
+      mediaRecorder.start();
+      setMediaRecorder(mediaRecorder);
+
+      mediaRecorder.ondataavailable = (event) => {
+        const audioBlob = new Blob([event.data], { type: "audio/wav" });
+        const audioURL = URL.createObjectURL(audioBlob);
+        setAudioURL(audioURL);
+        setAudioBlob(audioBlob);
+      };
+
+      draw(analyser, dataArray);
+    } catch (err) {
+      console.error("Error accessing media devices.", err);
+    }
+  };
+
+  const stopRecording = () => {
+    if (mediaRecorder) {
+      mediaRecorder.stop();
+    }
+    if (audioContext) {
+      audioContext.close();
+    }
+    setAudioContext(null);
+    setAnalyser(null);
+    setMediaRecorder(null);
+    setDataArray(null);
+  };
+
+  const draw = (analyser: AnalyserNode, dataArray: Uint8Array) => {
+    if (!canvasRef.current) return;
+
+    const canvasCtx = canvasRef.current.getContext("2d");
+    if (!canvasCtx) return;
+
+    const drawVisualizer = () => {
+      if (!analyser || !isRecording || !canvasRef.current) return;
+
+      requestAnimationFrame(drawVisualizer);
+
+      analyser.getByteFrequencyData(dataArray);
+
+      canvasCtx.clearRect(
+        0,
+        0,
+        canvasRef.current!.width,
+        canvasRef.current!.height
+      );
+
+      canvasCtx.fillStyle = "transparent";
+      canvasCtx.fillRect(
+        0,
+        0,
+        canvasRef.current!.width,
+        canvasRef.current!.height
+      );
+
+      const maxAmplitude = Math.max(...dataArray);
+      if (maxAmplitude < 100) {
+        return;
+      }
+
+      const barWidth = (canvasRef.current!.width / dataArray.length) * 2.5;
+      let barHeight;
+      let x = 0;
+
+      for (let i = 0; i < dataArray.length; i++) {
+        barHeight = dataArray[i] / 2;
+
+        canvasCtx.fillStyle = "#157EFF";
+        canvasCtx.fillRect(
+          x,
+          canvasRef.current!.height - barHeight,
+          barWidth,
+          barHeight
+        );
+
+        x += barWidth + 1;
+      }
+    };
+
+    drawVisualizer();
+  };
+
+  return (
+    <div className="w-full  mt-[10px]">
+      {!isRecording && !audioURL && (
+        <button
+          className="mt-[10px]  ml-[50px]"
+          onClick={() => setIsRecording(true)}
+        >
+          <FaMicrophone className="text-figmaBlue text-[30px]" />
+        </button>
+      )}
+
+      {isRecording && (
+        <div className="flex gap-10 h-[80px] items-center w-full py-[24px]  rounded-md px-[10px] bg-[#EFEFEF]">
+          <button>
+            <span
+              onClick={() => setIsRecording(false)}
+              className="w-[35px] h-[35px] flex justify-center items-center rounded-full bg-[#e61313]"
+            >
+              <FaStop className="text-white text-[16px]" />
+            </span>
+          </button>
+
+          <div className="w-full p-[10px]">
+            <canvas className="h-[80px] w-full" ref={canvasRef}></canvas>
+          </div>
+
+          <p className="text-figmaGrayShade">00:00/00:00</p>
+        </div>
+      )}
+
+      {audioURL && (
+        <div className="w-full">
+          <AudioPlayer audio={audioURL} />
+        </div>
+      )}
+    </div>
+  );
 };
 
-export default VoiceRecorder;
+export default AudioVisualizer;
