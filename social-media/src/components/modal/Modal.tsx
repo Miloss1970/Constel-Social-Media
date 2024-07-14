18c4cee64c1427358df768256b52119634d750df
Miloss1970 <134635415+Miloss1970@users.noreply.github.com>
2024-07-14 16:35:02 +0200

    create modal

--- social-media/src/components/modal/Modal.tsx
+++ social-media/src/components/modal/Modal.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
-const Modal = () => {
-  return <div>Modal</div>;
+interface ModalProps {
+  show: boolean;
+  closeModal: () => void;
+  children?: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({ show, closeModal, children }) => {
+  if (!show) return null;
+
+  return ReactDOM.createPortal(
+    <div className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+      <div
+        onClick={closeModal}
+        className="fixed top-0 left-0 right-0 bottom-0 bg-black opacity-30"
+      ></div>
+
+      <div className="relative z-[40] bg-white  rounded-lg">{children}</div>
+    </div>,
+    document.body
+  );
 };
 
 export default Modal;
