6bacaa168ee8e3dce4cea1bbc68a779b18777730
Miloss1970 <134635415+Miloss1970@users.noreply.github.com>
2024-07-14 17:04:13 +0200

    create Post

--- social-media/tailwind.config.js
+++ social-media/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        figmaRed: "#FF1515",
+
+        figmaBlue: "#157EFF",
+        figmaBlueShade: "#005BCA",
+
+        figmaGray: "#F9F9F9",
+        figmaGrayLight: "#D9D9D9",
+
+        figmaGrayShade: "#A6A6A6",
+        figmaGrayShade2: "#848484",
+      },
+    },
   },
   plugins: [],
 };
