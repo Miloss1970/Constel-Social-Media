1bf4e8156e7bad2d325259b69526449d272e49d8
Miloss1970 <134635415+Miloss1970@users.noreply.github.com>
2024-07-14 16:33:54 +0200

    create comment and like button

--- social-media/src/components/buttonComment/ButtonComment.tsx
+++ social-media/src/components/buttonComment/ButtonComment.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { FaRegComment } from "react-icons/fa";
 
-const ButtonComment = () => {
-  return <div>ButtonComment</div>;
+type ButtonCommentProps = {
+  type: string;
+  action?: () => void | null;
+  comments: number;
+};
+
+const ButtonComment: React.FC<ButtonCommentProps> = ({
+  type,
+  action,
+  comments,
+}) => {
+  const handleClick = () => {
+    if (type === "modal" && action) {
+      action();
+    }
+  };
+
+  return (
+    <button
+      className="flex gap-2 items-center px-[20px] py-[4px] rounded-lg text-figmaGrayShade bg-figmaGrayLight hover:text-white hover:bg-figmaGrayShade transition-all duration-300"
+      onClick={handleClick}
+    >
+      <FaRegComment /> <span>{comments}</span>
+    </button>
+  );
 };
 
 export default ButtonComment;
