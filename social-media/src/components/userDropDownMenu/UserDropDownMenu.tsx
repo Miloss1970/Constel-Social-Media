d91e69be0314f9741e59f3f526b81183f07be8f0
Miloss1970 <134635415+Miloss1970@users.noreply.github.com>
2024-07-17 14:03:35 +0200

    create user dropdown

--- social-media/src/components/userDropDownMenu/UserDropDownMenu.tsx
+++ social-media/src/components/userDropDownMenu/UserDropDownMenu.tsx
@@ -1,7 +1,32 @@
 import React from "react";
-
-const UserDropDownMenu = () => {
-  return <div>UserDropDownMenu</div>;
+import { MdLogout } from "react-icons/md";
+import { IoPerson } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
+const UserDropDownMenu: React.FC = () => {
+  const navigate = useNavigate();
+  const logOut = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+  return (
+    <div className="w-[200px] p-3 absolute top-[70px] bg-[#F9F9F9] right-[80px] rounded-lg">
+      <p className="flex gap-2 items-center mb-[15px] text-[16px] font-bold hover:bg-[#A6A6A6] p-1 rounded-md cursor-pointer">
+        <span className="w-[35px] h-[35px] flex justify-center items-center rounded-full bg-white">
+          <IoPerson className="text-blue-500 text-[20px]" />
+        </span>
+        My Profile
+      </p>
+      <p
+        onClick={logOut}
+        className="flex gap-2 items-center  text-[16px] font-bold hover:bg-[#A6A6A6] p-1 rounded-md cursor-pointer"
+      >
+        <span className="w-[35px] h-[35px] flex justify-center items-center rounded-full bg-white">
+          <MdLogout className="text-blue-500 text-[20px]" />
+        </span>
+        Log Out
+      </p>
+    </div>
+  );
 };
 
 export default UserDropDownMenu;
